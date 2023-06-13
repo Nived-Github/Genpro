e7f06d29813ba74bf0e28236ca0c076a71b75c76
Nived-Github <48906332+Nived-Github@users.noreply.github.com>
2023-06-13 14:45:34 +0530

    genpro.py

--- scripts/genpro.py
+++ scripts/genpro.py
@@ -3,28 +3,13 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path="../chromedriver.exe")
 driver.get("https://www.saucedemo.com/")
 sleep(2)
-
-# # Configure Chrome options
-# chrome_options = Options()
-# chrome_options.add_argument("--start-maximized")  # Maximize the browser window
-
-# Find and enter login credentials
 username_field = driver.find_element("id", "user-name")
 username_field.send_keys("secret_sauce")
 password_field = driver.find_element("id", "password")
 password_field.send_keys("secret_sauce")
 password_field.submit()
 
-# Add cases to check input field validations
-# TODO: Add your test cases here
-
-# Sort the products in price low to high order
-# TODO: Implement sorting of products
-
-# Add all items to the cart
-# TODO: Implement adding items to the cart
-
-# Go to the Cart page and remove items with price <$15
+# price <$15
 driver.find_element("class", "shopping_cart_link").click()
 # TODO: Implement removal of items with price <$15
 
